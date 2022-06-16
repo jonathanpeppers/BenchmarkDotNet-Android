fc912a35e6f5254c9c0e41675d53a7a74780d775
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-06-16 15:19:09 -0500

    Use C# 10

--- SharedBenchmarks/MathBenchmark.cs
+++ SharedBenchmarks/MathBenchmark.cs
@@ -2,35 +2,34 @@
 using BenchmarkDotNet.Order;
 using System;
 
-namespace SharedBenchmarks
-{
-	/// <summary>
-	/// Based on: https://github.com/dotnet/maui/issues/3100
-	/// </summary>
-	[MemoryDiagnoser]
-	[Orderer(SummaryOrderPolicy.FastestToSlowest)]
-	public class MathBenchmark
-	{
-		const int length = 1024;
-		readonly float[] data = new float[length];
-		readonly Random random = new Random();
+namespace SharedBenchmarks;
 
-		[Benchmark]
-		public void RandomNext()
+/// <summary>
+/// Based on: https://github.com/dotnet/maui/issues/3100
+/// </summary>
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class MathBenchmark
+{
+	const int length = 1024;
+	readonly float[] data = new float[length];
+	readonly Random random = new Random();
+
+	[Benchmark]
+	public void RandomNext()
+	{
+		double value = 300;
+		for (int i = 0; i < length; i++)
 		{
-			double value = 300;
-			for (int i = 0; i < length; i++)
+			if (random.NextDouble() > .5)
 			{
-				if (random.NextDouble() > .5)
-				{
-					value += random.NextDouble();
-				}
-				else
-				{
-					value -= random.NextDouble();
-				}
-				data[i] = (float)Math.Round(value, 2) * 2.625f;
+				value += random.NextDouble();
 			}
+			else
+			{
+				value -= random.NextDouble();
+			}
+			data[i] = (float)Math.Round(value, 2) * 2.625f;
 		}
 	}
 }
