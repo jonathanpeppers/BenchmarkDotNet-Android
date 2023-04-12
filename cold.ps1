f509b9fc4006e4311add1589b1061784f172dd3f
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-04-12 10:10:12 -0500

    Update cold.ps1

--- cold.ps1
+++ cold.ps1
@@ -4,7 +4,13 @@ param
     [string] $version
 )
 
+$params = "-bl", "-c", "Release", "-r", "android-arm64", "-p:Targeting=$version", "-p:ColdStart=true"
+
+& dotnet build ./DotNetRunner/DotNetRunner.csproj -t:Rebuild $params
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 for ($i = 0; $i -lt 5; $i++)
 {
-    & dotnet build ./DotNetRunner/DotNetRunner.csproj -bl -c Release -r android-arm64 -t:Benchmark -p:Targeting=$version -p:ColdStart=true | grep 'Info_Invoke \|' -CaseSensitive
+    & dotnet build ./DotNetRunner/DotNetRunner.csproj -t:Benchmark $params | grep 'Info_Invoke \|' -CaseSensitive
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 }
