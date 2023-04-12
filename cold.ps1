42bc1e1041b588f1525c2bbdeaba7b796820e589
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-04-12 10:22:38 -0500

    Update cold.ps1

--- cold.ps1
+++ cold.ps1
@@ -9,7 +9,7 @@ $params = "-bl", "-c", "Release", "-r", "android-arm64", "-p:Targeting=$version"
 & dotnet build ./DotNetRunner/DotNetRunner.csproj -t:Rebuild $params
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-for ($i = 0; $i -lt 5; $i++)
+for ($i = 0; $i -lt 10; $i++)
 {
     & dotnet build ./DotNetRunner/DotNetRunner.csproj -t:Benchmark $params | grep 'Info_Invoke \|' -CaseSensitive
     if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
