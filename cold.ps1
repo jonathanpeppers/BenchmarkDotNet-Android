83984c3ff70dcb98ac2ea250791f2707a67ce9ff
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-04-12 10:03:16 -0500

    Update cold.ps1

--- cold.ps1
+++ cold.ps1
@@ -4,7 +4,7 @@ param
     [string] $version
 )
 
-for (($i = 0), ($j = 0); $i -lt 5; $i++)
+for ($i = 0; $i -lt 5; $i++)
 {
     & dotnet build ./DotNetRunner/DotNetRunner.csproj -bl -c Release -r android-arm64 -t:Benchmark -p:Targeting=$version -p:ColdStart=true | grep 'Info_Invoke \|' -CaseSensitive
 }
